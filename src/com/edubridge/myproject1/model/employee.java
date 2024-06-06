75749ab0b9141e15240b6d2407ad4c0f3e23a8c3
cheguntla-naveen <cheguntlanaveen123@gmail.com>
2024-06-06 11:07:20 +0530

    Employee is Updated

--- src/com/edubridge/myproject1/model/employee.java
+++ src/com/edubridge/myproject1/model/employee.java
@@ -5,5 +5,23 @@ public class employee {
 	private int employeeId;
 	private String employeeName;
 	private long employeeMobile;
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public long getEmployeeMobile() {
+		return employeeMobile;
+	}
+	public void setEmployeeMobile(long employeeMobile) {
+		this.employeeMobile = employeeMobile;
+	}
 	
 }
